71edfa3a94349e3c97499902e9cb21ec643b5325
MYB-Dev <mybdev20@gmail.com>
2020-07-02 02:01:52 +0100

    deploy 2 to heroku

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/books', books);
 
-const PORT = 5000;
+const PORT = 80;
 
 app.listen(PORT, () => {
   console.log(`Api listening on http://localhost:${PORT}`);
