1b71a85fe9751591101139db69729250e82b835a
MYB-Dev <mybdev20@gmail.com>
2020-07-02 01:56:15 +0100

    deploy to heroku

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const books = require('./router/books');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/books', books);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Api listening on http://localhost:${PORT}`);
+});
---
+++ router/books.js
@@ -0,0 +1,60 @@
+var express = require('express');
+const monk = require('monk');
+var router = express.Router();
+
+const mongoURI =
+  'mongodb+srv://libraiumApi:LVGNBhEobBR5qUIY@librarium-j4qij.mongodb.net/libraryApi?retryWrites=true&w=majority';
+
+const db = monk(mongoURI);
+const books = db.get('books');
+
+function isValideBook(book) {
+  return (
+    book.title &&
+    book.title.toString().trim() !== '' &&
+    book.author &&
+    book.author.toString().trim() !== '' &&
+    book.isbn &&
+    book.isbn.toString().trim() !== ''
+  );
+}
+
+router.post('/', (req, res) => {
+  if (isValideBook(req.body)) {
+    const newBook = {
+      title: req.body.title.toString(),
+      author: req.body.author.toString(),
+      isbn: req.body.isbn.toString(),
+      published: new Date(),
+      available: true,
+    };
+
+    books
+      .insert(newBook)
+      .then((createdBook) => {
+        res.json(createdBook);
+      })
+      .catch((err) => {
+        res.json(err);
+      })
+      .then(() => db.close());
+  } else {
+    res.status(422);
+    res.json({
+      msg: 'Invaled information ???',
+    });
+  }
+});
+router.get('/', (req, res) => {
+  books.find().then((books) => {
+    res.json(books);
+  });
+});
+
+router.get('/search', (req, res) => {
+  books.find(req.query).then((book) => {
+    res.json(book);
+  });
+});
+
+module.exports = router;
